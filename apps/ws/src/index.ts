4a5004efae864875d4df08b501e41f5ff31c012a
Ashutoshpadhi629 <padhiashutosh106@gmail.com>
2024-09-17 18:51:34 +0530

    fixed/package-file

--- apps/ws/src/index.ts
+++ apps/ws/src/index.ts
@@ -1,1 +1,9 @@
-console.log("hello world");
+import { WebSocketServer } from "ws";
+
+const port = process.env.PORT ? +process.env.PORT : 8080;
+const wss = new WebSocketServer({ port });
+
+
+wss.on('open' , ()=> {
+    console.log("connected")
+})
